8df666c0a54e163e473bd37e9e863a1ee8ab2b01
Smith225 <smithrf123@gmail.com>
2023-08-25 17:40:43 +0530

    Modified category section with flexbox property

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,41 +6,41 @@ function Category() {
 
             <link rel="stylesheet" href={require("../css/styles.css")} />
 
-            <div className="row gx-0">
+            <div className="catBox">
 
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" alt="img" />
                     <p>Top Offers</p>
                 </div>
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100" alt="img" />
                     <p>Grocery</p>
                 </div>
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" alt="img" />
                     <p>Mobile</p>
                 </div>
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100" alt="img" />
                     <p>Fashion</p>
                 </div>
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100" alt="img" />
                     <p>Electronics</p>
                 </div>
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/ee162bad964c46ae.png?q=100" alt="img" />
                     <p>Home</p>
                 </div>
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100" alt="img" />
                     <p>Appliances</p>
                 </div>
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100" alt="img" />
                     <p>Travel</p>
                 </div>
-                <div className="col col-md-2">
+                <div className="catItem">
                     <img src="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100" alt="img" />
                     <p>Beauty, Toys & More</p>
                 </div>
